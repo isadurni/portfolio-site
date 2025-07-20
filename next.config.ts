0a565e8262c732a720e1ca04c69d833d849ba235
Ignacio Sadurni <156858270+isadurni@users.noreply.github.com>
2025-07-20 11:54:42 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+const nextConfig: NextConfig = {
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
