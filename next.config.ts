c796c3c7231bc085d04a37dc63238707d0b3a9ba
Ignacio Sadurni <156858270+isadurni@users.noreply.github.com>
2025-07-20 11:50:22 -0400

    Update next.config.ts
    Escape lint errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
